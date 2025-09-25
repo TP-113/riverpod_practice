7ded1b722f43ad1af6521ffdd542d18b31ba94bd
TP-113 <infinite1dream@gmail.com>
2025-09-25 16:34:45 +0900

    freezedの回答

---
+++ lib/controller/user/user_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userControllerHash() => r'61a585eb43a6f42546011c57a352f6c059c2df4e';
+
+/// See also [UserController].
+@ProviderFor(UserController)
+final userControllerProvider =
+    AutoDisposeNotifierProvider<UserController, User>.internal(
+      UserController.new,
+      name: r'userControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$userControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$UserController = AutoDisposeNotifier<User>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
